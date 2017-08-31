31340224d4b431da933078711e3135154daa8a54
Apinun <apinun.wong@gmail.com>
2017-08-31 21:49:52 +0700

    firsisrst commit

---
+++ DeeplinkTutorial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DeeplinkTutorial
+//
+//  Created by Apinun Wongintawang on 8/29/17.
+//  Copyright © 2017 Apinun Wongintawang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,21 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'DeeplinkTutorial' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+  pod 'Firebase/Core'
+  pod 'Firebase/DynamicLinks'
+  # Pods for DeeplinkTutorial
+
+  target 'DeeplinkTutorialTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'DeeplinkTutorialUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
